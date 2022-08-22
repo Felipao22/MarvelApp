5abb3578a8ade39f8f2ef8cb34f61aad036da646
FelipeAviani <felipeaviani@gmail.com>
2022-08-22 19:48:11 -0300

    Boiler plate

---
+++ components/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
